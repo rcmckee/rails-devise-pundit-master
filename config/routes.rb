af36d4cf53b7fcee10e5924ba68f7c308be8f09c
rcmckee <rmckee8@gmail.com>
2014-10-20 16:49:29 -0500

    scheduling for signed in users, pages for stripe
    stripe is probably functional but I would need to do some other things.
    but I used the scheduling software to do stripe.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
+
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'schedule' => 'pages#schedule'
   get 'visitors' => 'visitors#index'
   devise_for :users
   resources :users
+  resources :charges
 end
